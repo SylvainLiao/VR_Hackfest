fcf49c03a7719683891414a0df7a17247c73ebdb
Ｋ <k79k06k02k@gmail.com>
2016-08-26 20:29:37 +0800

    music fight

---
+++ Assets/Scripts/Enemy/EnemyStateMachineBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStateMachineBehaviour : StateMachineBehaviour
+{
+
+	public GameObject particle;
+	public float radius;
+	public float power;
+
+	protected GameObject clone;
+
+	override public void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+	}
+
+	override public void OnStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+	}
+
+	override public void OnStateUpdate (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+	}
+
+	override public void OnStateMove (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+	}
+
+	override public void OnStateIK (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+	}
+}
