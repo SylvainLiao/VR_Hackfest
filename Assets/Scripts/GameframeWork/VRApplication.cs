96f32c286c8aee98574a9e9d5c425644463035b6
Sylvain <sylvain770121@gmail.com>
2016-08-26 12:17:54 +0800

    project

---
+++ Assets/Scripts/GameframeWork/VRApplication.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRApplication: MonoBehaviour
+{
+    private static VRApplication m_instance;
+    public static VRApplication Instance
+    {
+        get
+        {
+            return (m_instance == null) ? new VRApplication() : m_instance;
+        }
+    }
+
+    private bool m_IsAllDataReady = false;
+    [HideInInspector]
+    public bool IsAllDataReady { get { return m_IsAllDataReady; } }
+
+    public bool GamePause = false;
+    //--------------------------------------------------------------------------------------
+    private void Awake()
+    {
+        m_instance = this;
+        Initial();
+    }
+    //--------------------------------------------------------------------------------------
+    // Use this for initialization
+    private void Start()
+    {
+        //Add All State At Here 
+
+        m_IsAllDataReady = true;
+    }
+    //--------------------------------------------------------------------------------------
+    private void Initial()
+    {
+    }
+    //--------------------------------------------------------------------------------------
+    private void Update()
+    {
+        if (GamePause)
+            return;
+    }
+}
