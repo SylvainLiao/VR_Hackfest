e7badddf0c92b209e10d279cb18414977c44f7c1
amidofu <amidofulue@gmail.com>
2016-08-28 12:58:46 +0800

    modify opening Animation

---
+++ Assets/Scripts/GameSceneControl/GameSceneController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSceneController : MonoBehaviour {
+    public GameObject OpeningAnimationRoot;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartOpening()
+    {
+        OpeningAnimationRoot.SetActive(true);
+    }
+
+    public void EndOpening()
+    {
+
+    }
+}
